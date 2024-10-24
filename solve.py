90915a76e9f167f981e64ff5b5dfaf8f58217af6
Anastasia Narbekova <laska3121@gmail.com>
2024-10-24 22:25:45 +0300

    Finish2

--- solve.py
+++ solve.py
@@ -1,19 +1,19 @@
-with open('1.txt', encoding='utf-8') as file_1, \
-     open('2.txt', encoding='utf-8') as file_2, \
-     open('3.txt', encoding='utf-8') as file_3:
+import os
+def read_files_in_directory(directory):
     files_data = {}
-    files = [file_1, file_2, file_3]
-    for i, file in enumerate(files):
-        lines = file.readlines()
-        line_count = len(lines)
-        file_name = f"{i + 1}.txt"
-        content = f"{file_name}\n{line_count}\n" + ''.join(lines)
-        files_data[line_count] = content
-sorted_files_data = dict(sorted(files_data.items()))
-with open('4.txt', 'w', encoding='utf-8') as file_4:
-    for content in sorted_files_data.values():
-        file_4.write(content)
-
-with open ('4.txt') as file_4:
-    data = file_4.read()
-    print(data)
+    for filename in os.listdir(directory):
+        if os.path.isfile(os.path.join(directory, filename)) and filename.endswith('.txt'):
+            with open(os.path.join(directory, filename), encoding='utf-8') as file:
+                lines = file.readlines()
+                line_count = len(lines)
+                content = f"{filename}\n{line_count}\n" + ''.join(lines)
+                files_data[line_count] = content
+    sorted_files_data = dict(sorted(files_data.items()))
+    with open(os.path.join(directory, '4.txt'), 'w', encoding='utf-8') as file_4:
+        for content in sorted_files_data.values():
+            file_4.write(content)
+    with open(os.path.join(directory, '4.txt'), 'r', encoding='utf-8') as file_4:
+        data = file_4.read()
+        print(data)
+directory_path = '/Users/anastasianarbekova/Desktop/Task_3'
+read_files_in_directory(directory_path)
